b66db6e8ba1a17883180db3d3e691accdc803b1d
Eduardo <tuchix2@gmail.com>
2015-03-09 06:10:58 -0300

    Template Engines
    Updated

--- 04-templates-engines/json.js
+++ 04-templates-engines/json.js
@@ -27,43 +27,37 @@ Profile[3].Courses;
 
 
 
-//----------------With Handlebars----------------------//
+
 $(document).ready(function(){
-var fuente = $('#nuestra-plantilla').html();
+var fuente = $('#plantilla2').html();
 var plantilla = Handlebars.compile(fuente);
 
 var datos = {
-    Profile: [
-        {nombre: 'Im Michael and i am 25 years old.'},
-        {education: 'I studied programming in Ultimatum School of informatics.'},
-        {experience: 'I worked 15 years in Google as CEO director.'},
-        {courses: 'Web Design, Android development and videogame development.'},
-    ],
+    Profile: {
+        nombre: 'Im Michael and i am 25 years old.',
+        education: 'I studied programming in Ultimatum School of informatics.',
+        experience: 'I worked 15 years in Google as CEO director.',
+        courses: 'Web Design, Android development and videogame development.',
+    },
 };
+
+//----------------With Handlebars----------------------//
 var html = plantilla(datos);
-
 $('#container2').html(html);
-});
-//--------------------------------------------------//
-
 
 
 //-----------------With Underscore-------------------//
-$(document).ready(function(){
-var datos2 =[
-  {
-        "nombre": "Im Michael and i am 25 years old.",
-        "education": "I studied programming in Ultimatum School of informatics.",
-        "experience": "I worked 15 years in Google as CEO director.",
-        "courses": "Web Design, Android development and videogame development.",
-   }
-  ];     
-  _.each(datos2, function(element){
-    var div=$("#container3");
-    div.append("<h2>About Me</h2>"+"<div>"+element.nombre+"</div>");
-    div.append("<h2>Education</h2>"+"<div>"+element.education+"</div>");
-    div.append("<h2>Experience</h2>"+"<div>"+element.experience+"</div>");
-    div.append("<h2>Courses</h2>"+"<div>"+element.courses+"</div>");
-  });
+var plantilla3 = _.template($('#plantilla3').html()); 
+$('body').append(plantilla3(datos));
+
+
+//-----------------With Dust-------------------//
+
+var compiled = dust.compile($('#plantilla4').html(), "intro"); 
+dust.loadSource(compiled);
+dust.render("intro", datos, function(err, out) {
+        $('#container4').html(out);
+});
 
 });
+//--------------------------------------------------//
